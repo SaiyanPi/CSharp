0d6c4f432e99c9057c5666457480e241876c86d3
neerajan rai <zlamsong6@gmail.com>
2024-02-28 18:35:04 +0545

    added method/function cann and array-to-function

---
+++ ArrayToFunction/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace ArrayToFunction
+{
+    class Program
+    {
+        //printing array values
+        static void method(int[] arra)
+        {
+            Console.WriteLine("printing array members");
+            for(int i = 0; i < arra.Length; i++)
+            {
+                Console.WriteLine(arra[i]);
+            }
+        }
+
+        //printing minimun array vallue
+        static void minimum(int[] arr)
+        {
+            int min = arr[0];
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"Minimun value is {min}");
+        }
+
+        //printing maximum value
+        static void maximum(int[] ar)
+        {
+            int max = ar[0];
+            for (int i = 1; i < ar.Length; i++)
+            {
+                if (max < ar[i])
+                {
+                    max = ar[i];
+                }
+            }
+            Console.WriteLine($"Maximum array value is {max}");
+        }
+        static void Main(string[] args)
+        {
+            int[] arrayone = { 10, 12, 31, 9 };
+
+            method(arrayone);
+
+            minimum(arrayone);
+
+            maximum(arrayone);
+        }
+    }
+}
+
+ 
---
+++ methodCall/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MethodCall;
+class Program
+{
+    
+    public void FunctionOne(int x)
+    {
+        x *= x;
+        Console.WriteLine($"value of x inside the first function is {x}");
+    }
+
+    static void Message()
+    {
+        Console.WriteLine("This is static method");
+    }
+
+    public void Functiontwo(ref int x)
+    {
+        x *= x;
+        Console.WriteLine($"Value inside the second function is {x}");
+    }
+
+
+    static void Main(string[] args)
+    {
+        int x = 5;
+        Program progObject = new Program();
+
+        Console.WriteLine($"value of x before method call is {x}");
+        progObject.FunctionOne(x);
+        Console.WriteLine($"value of x after function call is {x}");
+
+        Message(); //object reference is not needed for static method.
+        
+        Console.WriteLine($"value of x before second function call is {x}");
+        progObject.Functiontwo(ref x);
+        Console.WriteLine($"value of x after second function call is {x}");
+    }
+}
