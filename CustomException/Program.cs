eb146c92bb2bd5f8b27b69a536ea0d2327a50cc8
neerajan rai <zlamsong6@gmail.com>
2024-03-17 17:27:34 +0545

    added custom exception

---
+++ CustomException/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CustomException
+{
+    public class InvalidAgeException : Exception //Custom exception need to inherit Exception class
+    {
+        public InvalidAgeException(string message) : base(message)
+        {
+           
+           
+        }
+    }
+
+    class TestCustomException
+    {
+        static void Validate(int age)
+        {
+            if (age < 18)
+            {
+                throw new InvalidAgeException("Sorry you are under aged.");
+            }
+
+            else
+            {
+                Console.WriteLine("you can proceed.");
+            }
+                
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter your age");
+            int umer = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Validate(umer);
+            }
+            catch(InvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
