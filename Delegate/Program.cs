60465db764da8ac01bcfc1fc140cd48a6a36b82a
neerajan rai <zlamsong6@gmail.com>
2024-03-18 18:38:38 +0545

    added delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegate
+{
+    //DECLARING DELEGATE, DELEGATE SIGNATURE = int NumberChanger(int n)
+    delegate int NumberChanger(int n);
+    class DelegateTest
+    {
+        static int num = 10;
+        //METHOD SIGNATURE MUST  MATCH WITH THE DELEGATE SIGNATURE
+        public static int AddNum(int p) //method signature = int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MulNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            //CREATING DELEGATE INSTANCES/OBJECTS AND SETTING A TARGET METHOD
+            NumberChanger nc1 = new NumberChanger(AddNum); 
+            //NumberChanger nc1 = AddNum; //targeting a method without creating a delegate object
+            NumberChanger nc2 = new NumberChanger(MulNum);
+            //NumberChanger nc2 = MulNum;
+
+
+            //CALLING/INVOKING THE METHODS USING DELEGATE OBJECTS
+            nc1(5); //OR, nc1.Invoke(5);
+            Console.WriteLine("value of Num: {0}", getNum()); //10+5 = 15| new num =15
+            nc2(5);
+            Console.WriteLine("value of Num: {0}", getNum()); //15*5 = 75
+            Console.ReadKey();
+
+        }
+    }
+}
