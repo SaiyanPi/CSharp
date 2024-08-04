450ed8f506c96e7c40fc08ef5e5fbf7b67b76f8b
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-08-04 19:37:09 +0545

    added - method overriding

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+
+    public class Vehicle
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("I am driving a vehicle");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("I am driving a Car");
+        }
+    }
+
+    public class Toyota : Car
+    {
+        public override void Drive()
+        { 
+            Console.WriteLine("I am driving Toyota");            
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Vehicle vechInstance = new Car();
+        vechInstance.Drive(); // I am driving a Car.
+
+        Car carInstance = new Toyota();
+        carInstance.Drive(); // I am driving a Toyota.
+
+        //Toyota toyotaInstance = new Vehicle(); // error
+        //Toyota toyotaInstance = new Car(); // error
+    }
+
+}
