7ac8fd268f6a61f28c7719678523def60df22345
Neerajan Rai <141233559+SaiyanPi@users.noreply.github.com>
2024-11-11 15:23:15 +0545

    Update ValueReference type

--- ValurReferenceType/Program.cs
+++ ValurReferenceType/Program.cs
@@ -13,23 +13,23 @@ namespace ValueReference
             //Value type behaviour
             int x = 14;
             int y = x;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            Console.WriteLine(x); //14
+            Console.WriteLine(y); //14
                        
             y = 15;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            Console.WriteLine(x); //14
+            Console.WriteLine(y); //15
 
             //Reference type behaviour
             Demo obj1 = new Demo();
             Demo obj2 = obj1;
-            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}");
+            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}"); //21
 
             obj2.age = 27;
-            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}");
+            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}"); //27
 
             obj1.age = 32;
-            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}");
+            Console.WriteLine($"obj1.age = {obj1.age} obj2.age = {obj2.age}"); //32
         }
     }
 }
